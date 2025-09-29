5a0b8148d48036246580fb261df5c0422a63e8ce
Andrew Le <a.asterisk.le@gmail.com>
2025-09-29 08:49:56 -0500

    Forgot to link routing login from controller to route for updating question.

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../utils/jwt');
 const { createQuestion } = require('../controllers/questionController');
+const { updateQuestionStatus } = require('../dao/questionDAO');
 
 // Route middleware for every request to URLs past this point
 router.use(authenticate)
 // Route for question creation
 router.post(`/questions`, createQuestion);
 // Route for updating question
-router.patch(`/questions/:question_id`)
+router.patch(`/questions/:question_id`, updateQuestionStatus);
 
 module.exports = router;
