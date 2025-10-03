8bc380b978ae611ac2e627f819a541354207eea6
Andrew Le <a.asterisk.le@gmail.com>
2025-10-03 09:10:17 -0500

    Set up really basic admin panel to approve or deny questions.

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,24 +1,52 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Question {
+  questionId: string;
+  question: string;
+  status: string;
+  category: string;
+  type: string;
+  difficulty: string;
+}
 
 function Admin() {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNWNkNzVkNy1lOTI2LTQyYjctOTM0OS1lYmViNWEzYWJiNzAiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NTk0OTkwMTEsImV4cCI6MTc1OTU0MjIxMX0.Us2jIwACPxZhiI9eAvw3hQY6aBVBNJ0OxX9OScQ7KI4";
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-  axios
-    .get("http://localhost:3000/questions/status?status=pending", {
-      headers: {
-        // Due to login not being implemented yet, hard-code JWT token from Postman call here.
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => console.log(err));
+  useEffect(() => {
+      axios
+        .get("http://localhost:3000/questions/status?status=pending", {
+          headers: {
+            Authorization: `Bearer ${token}` 
+          },
+      })
+      .then((response) => {
+        setQuestions(response.data.questions); 
+      })
+        .catch((err) => console.error(err));
+  }, []); 
+
+  useEffect(()=> {
+    console.log("QUESTIONS: ", JSON.stringify(questions));
+  }, [questions])
+
 
   return (
     <>
-      <div>Admin</div>
+      <div>
+        <h1>PENDING:</h1>
+        <ul>
+          {questions.map((question) => (
+            <li key={question.questionId}>
+              {question.question} {question.status}
+              <button> Approve </button>
+              <button> Deny </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
